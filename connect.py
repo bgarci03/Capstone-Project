597c1507e7bb459c37d5b4f6e3ec7595a3f11c19
Brandon Garcia <125494283+bgarci03@users.noreply.github.com>
2025-01-21 16:37:49 +0000

    Google Sheets Connections

--- connect.py
+++ connect.py
@@ -2,4 +2,13 @@ import streamlit as st # type: ignore
 from streamlit_gsheets import GSheetsConnection # type: ignore
 
 def get_student_loc_data_db():
-  return st.connection("gsheets",type=GSheetsConnection).read(index_col="ID Number")
+  conn = st.connection("gsheets", type=GSheetsConnection)
+  return conn.read(spreadsheet=st.secrets["database"]["students_url"], index_col="ID Number")
+
+def get_approved_signers_db():
+  conn = st.connection("gsheets", type=GSheetsConnection)
+  return conn.read(spreadsheet=st.secrets["database"]["approved_signers_url"], index_col="Name")
+
+def get_upcoming_loc_labs_db():
+  conn = st.connection("ghseets", type=GSheetsConnection)
+  return conn.read(spreadsheet=st.secrets["database"]["upcoming_loc_labs_url"], index_col="School")
