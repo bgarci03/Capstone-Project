368fd3d27422d16603c9917b7b380a9d4aa89eac
Brandon Garcia <125494283+bgarci03@users.noreply.github.com>
2025-08-12 07:25:52 +0000

    Finalization

--- pages/admin.py
+++ pages/admin.py
@@ -17,5 +17,28 @@ generate_sidebar()
 st.title("Admin Panel")
 
 st.write(f"Welcome, {st.session_state.get("name")}! Manage Loss of Credit for students and view, add, or change events!")
+st.write("You can view every loss of credit lab/event in the **Manage Events** page in the sidebar with the Calendar!")
 
-st.write("Refresh the page to see new updated events!")
+col1, col2 = st.columns(2, border=True)
+
+with col1:
+  st.header("How to add an event!")
+  st.markdown(
+  """
+  1. *Head* to the page **Managed Events**
+  2. *Select* the **Add Event** tab at the top
+  3. *Fill out* the form
+  4. **Submit** the form
+  """)
+
+with col2:
+  st.header("How to delete an event!")
+  st.markdown(
+  """
+  1. *Head* to the page **Managed Events**
+  2. *Select* the **Delete Event** tab at the top
+  3. *Fill out* the first two inputs
+  4. *Look* for the event to delete
+  5. *Copy* the event information
+  6. **Submit** the form
+  """)
